0669c8eeee4acc514039cff14b742ebad2a883b7
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2025-12-04 18:50:43 -0300

    ajustes feitos

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,48 @@
 // Criar instância do modal
 const myModal = new bootstrap.Modal(document.getElementById("register-modal"));
+let logged = sessionStorage.getItem("Logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
 
 
-//Logar no sistema
+//LOGAR NO SISTEMA
+
+document.getElementById("login-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checkSession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account) { /*se não econtrou a conta*/
+        alert("Opps! Verifique o usúario ou a senha")
+        return;
+    }
+
+    if (account){
+        
+        if(account.password !== password) {
+            alert("Opps! Verifique se o usuario ou a senha estão errados.")
+            return;
+        }
+
+        saveSession(email, checkSession);
+
+        window.location.href = "home.html"; /*Se econtrar vai para home*/ 
+    }
+
+
+    
+    
+});
 
 
 
 
-// Evento de submit do formulário de cadastro
+// CRIAR CONTA
 document.getElementById("register-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
@@ -40,7 +75,40 @@ document.getElementById("register-form").addEventListener("submit", function(e) 
     myModal.hide();
 });
 
+
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("looged", session);
+        logged = session;
+    }
+
+    if(logged){
+        saveSession(logged, session);
+
+        window.location.href = "home.html" /*se o usuario clicar em permancer logado ele já jogo para home*/ 
+    }
+}
+
 // Função para salvar conta no localStorage
 function saveAccount(data) {
     localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession){
+    if (saveSession){
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("session", data);
+}
+
+
+function getAccount (key){
+    const account = localStorage.getItem(key);
+    if(account) {
+        return JSON.parse(account);
+    }
+
+    return "";
+
 }
