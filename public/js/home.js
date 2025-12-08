6a6408ac102288dd288f7dac03df7abca3d90ed7
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2025-12-08 17:56:10 -0300

    Já é possivel adicionar entradas e saidas

--- public/js/home.js
+++ public/js/home.js
@@ -10,6 +10,10 @@ let data = {
 
 // BOTÃO SAIR
 document.getElementById("button-logout").addEventListener("click", logout);
+// BOTÃO ADICIONAR LANÇAMENTO
+document.getElementById("transaction-button").addEventListener("click", function () {
+    window.location.href = "transactions.html";
+})
 
 // ADICIONAR LANÇAMENTO
 document.getElementById("transaction-form").addEventListener("submit", function (e) {
@@ -29,6 +33,10 @@ document.getElementById("transaction-form").addEventListener("submit", function 
 
     saveData(data);  // salva no localStorage
     e.target.reset(); // limpar formulário
+
+    getCashIn(); // atualizar lançamentos na tela
+    getCashOut(); // atualizar lançamentos na tela
+    getTotal();  // atualizar total na tela
 
     // 🔥 CORREÇÃO DO ERRO DE ARIA-HIDDEN
     document.activeElement.blur();  // tira o foco do botão que abriu o modal
@@ -62,6 +70,8 @@ function checkLogged() {
     }
 
     getCashIn();
+    getCashOut();
+    getTotal(); 
 }
 
 // FUNÇÃO SAIR
@@ -87,10 +97,103 @@ function getCashIn() {
         }
 
         for (let index = 0; index < limit; index++) {
-            console.log(index);
-            console.log(cashIn[index]);
+            cashInHtml +=
+                  `<div class="row mb-4">
+                          <div class="col-12">
+                            <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+                            <div class="container p-0">
+                              <div class="row">
+                                <div class="col-12 col-md-8">
+                                    <p>${cashIn[index].description}</p>
+                                </div>
+                                <div class="col-12 col-md-3 d-flex justify-content-end">
+                                    <p>${cashIn[index].date}</p>
+
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                          
+                          
+                          
+                        </div> `
         }
+
+        document.getElementById("cash-in-list").innerHTML = cashInHtml;
+  
+  
     }
+
+}
+
+
+
+
+// CASH OUT - EM DESENVOLVIMENTO
+function getCashOut() {
+    const transactions = data.transactions;
+
+    const cashIn = transactions.filter((item) => item.type === "2");
+
+    if (cashIn.length) {
+        let cashInHtml = ``;
+        let limit = 0;
+
+        if (cashIn.length > 5) {
+            limit = 5;
+        } else {
+            limit = cashIn.length;
+        }
+
+        for (let index = 0; index < limit; index++) {
+            cashInHtml +=
+                  `<div class="row mb-4">
+                          <div class="col-12">
+                            <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+                            <div class="container p-0">
+                              <div class="row">
+                                <div class="col-12 col-md-8">
+                                    <p>${cashIn[index].description}</p>
+                                </div>
+                                <div class="col-12 col-md-3 d-flex justify-content-end">
+                                    <p>${cashIn[index].date}</p>
+
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                          
+                          
+                          <div class="col-12">
+                            <button type="button" class="btn button-default">
+                              Ver todas
+                            </button>
+                          </div>
+                        </div> `
+        }
+
+        document.getElementById("cash-out-list").innerHTML = cashInHtml;
+  
+  
+    }
+
+}
+
+function getTotal() {
+    const transactions = data.transactions;
+    let total = 0;
+
+    transactions.forEach((item) => {
+        if (item.type === "1") {
+            total += item.value;
+        } else {
+            total -= item.value;
+        }
+    });
+
+    document.getElementById("total").innerHTML = `R$${total.toFixed(2)}`;
 }
 
 // SALVAR DADOS DO USUÁRIO
@@ -98,3 +201,6 @@ function saveData(data) {
     // Usa o email/login do usuário como chave
     localStorage.setItem(logged, JSON.stringify(data));
 }
+
+
+
